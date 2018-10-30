f1a44d8f22f77cc2ab85d60593ecc7f3b4227b6a
Adam <ithinkiamadam@gmail.com>
2018-10-30 19:12:56 -0400

    Change case handling, add methods, deal with multi-word strings

--- word-bastardizer.js
+++ word-bastardizer.js
@@ -11,33 +11,74 @@ function revertToOriginalWord() {
 }
 
 function swapLetters(s) {
-	let randIndex = Math.floor(Math.random() * s.length);
-	return s.substring(0, randIndex) + s.charAt(randIndex+1) + s.charAt(randIndex) + s.substring(randIndex + 2);
+	while(true) {
+		let randIndex = randIndexIn(s.substring(0, s.length-1));
+		let char1 = s.charAt(randIndex);
+		let char2 = s.charAt(randIndex + 1);
+		return s.substring(0, randIndex) + 
+		makeSameCase(char2, char1) + 
+		makeSameCase(char1, char2) + 
+		s.substring(randIndex + 2);
+	}
+	return s;
 }
 
 function swapVowel(s) {
-	let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+	let vowels = ["a", "e", "i", "o", "u"];
 	if(s.match("[aeiouAEIOU]")) {
 		while(true) {
-			let randIndex = Math.floor(Math.random() * s.length);
-			if(vowels.includes(s.charAt(randIndex))) {
-				return (s.substring(0, randIndex) + vowels[Math.floor(Math.random() * vowels.length)] + s.substring(randIndex + 1));
+			let randIndex = randIndexIn(s);
+			let randChar = s.charAt(randIndex);
+			if(vowels.includes(randChar.toLowerCase())) {
+				return (s.substring(0, randIndex) + 
+				makeSameCase(randElementOf(vowels), randChar) + 
+				s.substring(randIndex + 1));
 			}	
 		}
 	}
 	return s;
 }
 
+function randIndexIn(string) {
+	return Math.floor(Math.random() * string.length);
+} 
+
+function randElementOf(array) {
+	return  array[Math.floor(Math.random() * array.length)];
+}
+
+function makeSameCase(stringToChange, stringWithCase) {
+	if(stringToChange.toUpperCase() && stringWithCase.toUpperCase() ) {
+		if(stringWithCase == stringWithCase.toUpperCase()) {
+			return stringToChange.toUpperCase();
+		} else if(stringWithCase == stringWithCase.toLowerCase()) {
+			return stringToChange.toLowerCase();
+		} 
+	}
+	return stringToChange;
+}
+
+//TODO Switch voiced-unvoiced consonants
+//TODO Add a sliding scale of messed-up-ness
+
 function repeatLetter(s) {
-	let randIndex = Math.floor(Math.random() * s.length);
-	return s.substring(0, randIndex) + s.charAt(randIndex) + s.charAt(randIndex) + s.substring(randIndex + 1);
+	let randIndex = randIndexIn(s);
+	randChar = s.charAt(randIndex);
+	return s.substring(0, randIndex) + 
+	randChar + 
+	randChar.toLowerCase() + s.substring(randIndex + 1);
 }
 
 function messUpWord() {
-	var currWord = origWord;
-	currWord = repeatLetter(currWord);
-	currWord = swapLetters(currWord);
-	currWord = swapVowel(currWord);
-	textBox.value = currWord;
+	var currString = "";
+	let wordArr = origWord.split(" ");
+	for(let i = 0; i < wordArr.length; i++) {
+		let currWord = wordArr[i];
+		currWord = repeatLetter(currWord);
+		currWord = swapLetters(currWord);
+		currWord = swapVowel(currWord);
+		currString += currWord + " ";
+	}
+	textBox.value = currString;
 }
 
